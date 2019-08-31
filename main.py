32d6ee73d80b8e75ad0033120f40574338e50814
Jacob <jacob.b0617@gmail.com>
2019-08-31 11:46:05 -0400

    completes assignment

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name is Jacob Bailey')
+print('My favorite game is Bioshock Infinite')
+print('My only concern is getting back into the groove of coding for this class')
+print('I just want to learn more about what goes into creating the things I love and more about the industry in general.')
+print('stackoverflow number: user:12003507')
+print('github url: https://github.com/17jrb17')
